67056fb58fa37e721064de775709833d2d1a5789
Brian Donghee Shin <shindonghee@gmail.com>
2012-03-24 17:55:48 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodsquares::Application.config.secret_token = 'dc8070857d8e701dbfbbf2a50ff27ef85ef5a1d5afcb499744465d43568406685fd13b7256aebd1044c8bdcbbcd0e11358ad85c990fb525837af67bc644ca5b2'
