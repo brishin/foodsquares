356a9237728b6eecb4111774b748d5a643e085bb
Andrew Pope <drewii2ii@gmail.com>
2012-03-25 05:15:48 -0400

    Updated voting page, created basic quantity form

---
+++ app/controllers/voters_controller.rb
@@ -0,0 +1,10 @@
+class VotersController < ApplicationController
+  def index
+    @items = Item.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @items }
+    end
+  end
+ end
